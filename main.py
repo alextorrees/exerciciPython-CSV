6e74fdd5e33275c74cca2609a47542a81678966d
sjo <alejandro.torres.7e6@itb.cat>
2023-10-04 17:44:43 +0200

    change positions and format of height and weight

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import csv
 
-with open('basket_players.csv', 'r') as archivo_csv:
-    datos = csv.reader(archivo_csv, delimiter=';')
+def translate_position(position):
+    position_translations = {
+        'Point Guard': 'Base',
+        'Shooting Guard': 'Escorta',
+        'Small Forward': 'Aler',
+        'Power Forward': 'Ala-Pivot',
+        'Center': 'Pivot'
+    }
+    return position_translations.get(position, position)
 
-    for i, row in enumerate(datos):
-        name = row[0]
-        team = row[1]
-        postion = row[2]
-        height = row[3]
-        weight = row[4]
-        age = row[5]
-        print(f'{name};{team};{postion};{height};{weight};{age}')
+def print_player_info(name, team, position, height, weight, age):
+    print(f'{name};{team};{position};{height};{weight};{age}')
 
+def process_csv():
+    with open('basket_players.csv', 'r') as archivo_csv:
+        datos = csv.DictReader(archivo_csv, delimiter=';')
+        print("Nom;Equip;Posició;Altura;Pes;Edad")
+        for i, row in enumerate(datos):
+            name = row['Name']
+            team = row['Team']
+            position = translate_position(row['Position'])
+            height = round(float(row['Heigth']) * 2.54, 2)
+            weight = round(float(row['Weigth']) * 0.45, 2)
+            age = row['Age']
+            print_player_info(name, team, position, height, weight, age)
 
+def exercici_1():
+    process_csv()
+
+exercici_1()
 
 
